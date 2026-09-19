bda0b68e2bafd643a649aaa3e59677a6fb6cc5dc
Marlan Pather <info@seowebpixels.com>
2026-09-19 12:11:59 +0200

    Fix astro cloudflare adapter config and wrangler settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,9 @@ import cloudflare from '@astrojs/cloudflare';
 export default defineConfig({
   site: 'https://webdesignpros.co.za',
   output: 'server',
-  adapter: cloudflare(),
+  adapter: cloudflare({
+    imageService: 'passthrough',
+  }),
   integrations: [
     sitemap()
   ],
