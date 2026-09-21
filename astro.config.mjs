5bfc8069b10ae784b61efc1c98fbf26c4baa834c
Marlan Pather <info@seowebpixels.com>
2026-09-21 17:25:09 +0200

    Configure root wrangler.json and Cloudflare Pages SSR worker

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,11 @@ import cloudflare from '@astrojs/cloudflare';
 export default defineConfig({
   site: 'https://webdesignpros.co.za',
   output: 'server',
-  adapter: cloudflare(),
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true,
+    },
+  }),
   integrations: [
     sitemap()
   ],
