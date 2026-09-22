ae0d45dc4c24a9a99822071680abeea3923474bf
Marlan Pather <info@seowebpixels.com>
2026-09-22 12:37:03 +0200

    Three - Fix to display pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,6 @@ import cloudflare from '@astrojs/cloudflare';
 export default defineConfig({
   site: 'https://webdesignpros.co.za',
   output: 'static',
-  build: {
-    format: 'directory'
-  },
   integrations: [
     sitemap()
   ],
